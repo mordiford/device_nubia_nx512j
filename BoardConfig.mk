2d200ed196ffb89ed4b9241599c01480ee50c7d0
lindwurm <lindwurm.q@gmail.com>
2017-06-04 22:29:41 +0900

    Move Hardware info to product/info.mk
    from system.prop
    
    Signed-off-by: lindwurm <lindwurm.q@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,7 +20,6 @@ include device/nubia/nx512j/board/*.mk
 
 # Properties
 TARGET_OTA_ASSERT_DEVICE := NX512J,nx512j
-TARGET_SYSTEM_PROP += $(VENDOR_PATH)/system.prop
 
 # Inherit from proprietary files
 -include vendor/nubia/nx512j/BoardConfigVendor.mk
