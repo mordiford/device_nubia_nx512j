08a25e8b92c2130e20cb68ec9b5f930a81759088
lindwurm <lindwurm.q@gmail.com>
2018-03-17 23:13:04 +0900

    nx512j: AICPify

--- aicp.mk
+++ aicp.mk
@@ -15,10 +15,10 @@
 
 $(call inherit-product, device/nubia/nx512j/full_nx512j.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AICP stuff.
+$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_nx512j
+PRODUCT_NAME := aicp_nx512j
 BOARD_VENDOR := nubia
 PRODUCT_DEVICE := nx512j
 
